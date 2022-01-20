de8aaa80b360520f7adfc62486692eb98cc5b706
Hayden Couch <hcouch@lcs.on.ca>
2022-01-20 11:16:17 -0500

    Initial Commit

---
+++ Shared/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Shared
+//
+//  Created by Hayden Couch on 2022-01-20.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
